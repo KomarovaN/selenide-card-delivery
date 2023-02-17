46d050835cc4391d42a30ad294c4176a6c2a3702
Nadezhda Komarova <grukhinan@gmail.com>
2023-02-17 14:36:01 +0300

    Initial commit

---
+++ src/test/java/ru/netology/ui/CardTest.java
@@ -0,0 +1,35 @@
+package ru.netology.ui;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardTest {
+
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:7777");
+        SelenideElement form = $("form");
+        form.$("[data-test-id='city'] input").setValue("Киров");
+        form.$("[data-test-id='name'] input").setValue("Василий");
+        form.$("[data-test-id='phone'] input").setValue("+79270000000");
+        form.$("[data-test-id='agreement']").click();
+        form.$(byText("Забронировать")).click();
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+    }
+
+}
