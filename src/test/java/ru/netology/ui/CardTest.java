1e25107d57e967ba0de1f52eea873d0eb81993fc
Nadezhda Komarova <grukhinan@gmail.com>
2023-03-05 18:23:33 +0300

    Executed rework 1

--- src/test/java/ru/netology/ui/CardTest.java
+++ src/test/java/ru/netology/ui/CardTest.java
@@ -15,6 +15,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
@@ -24,13 +25,13 @@ import static com.codeborne.selenide.Selenide.*;
 public class CardTest {
     private String planningDate;
 
-    public String generateDate(int days) {
-        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    public String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern, Locale.ENGLISH));
     }
 
     @Test
     public void shouldSubmitRequest() {
-        planningDate = generateDate(4);
+        planningDate = generateDate(4, "dd.MM.yyyy");
 
         open("http://localhost:7777");
         SelenideElement form = $("form");
@@ -41,59 +42,46 @@ public class CardTest {
         form.$("[data-test-id='phone'] input").setValue("+79270000000");
         form.$("[data-test-id='agreement']").click();
         form.$(byText("Забронировать")).click();
+
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $(".notification__content")
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
+
     }
 
     @Test
     public void shouldSetCityFromListAndDateFromCalendar() {
         open("http://localhost:7777");
-        SelenideElement form = $("form");
 
         // Ввод двух букв в поле «Город», после чего выбор нужного города из выпадающего списка
-        form.$("[data-test-id='city'] input").setValue("Ки");
-        int count = $$("span.menu-item__control").size();
-        for (int i = 0; i < count; i++) {
-            String tmp = $$("span.menu-item__control").get(i).getText();
-            tmp = tmp.substring(0, 2);
-            if (tmp.equals($("[data-test-id='city'] input").getValue())) {
-                $$("span.menu-item__control").get(i).click();
-                break;
-            }
-        }
+        $("[data-test-id='city'] input").sendKeys("Ки");
+        $$(".popup_visible .menu-item").findBy(text("Киров")).click();
 
         // Выбор даты на неделю вперёд, начиная от текущей даты, через инструмент календаря
-        Calendar calendar = new GregorianCalendar();
-        calendar.add(Calendar.DAY_OF_MONTH, 7);                      // устнавливаем дату на неделю позже
-        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-        if (calendar.get(Calendar.DAY_OF_WEEK) == 7) {                      // если попали на сб., то перенос на пн.
-            calendar.add(Calendar.DAY_OF_MONTH, 2);
+        int addDays = 7;
+        if (generateDate(addDays, "E").equals("Sat")) {
+            addDays += 2;                                                 // если попали на сб., то перенос на пн.
+        } else if (generateDate(addDays, "E").equals("Sun")) {
+            addDays++;                                                    // если попали на вск., то перенос на пн.
         }
-        if (calendar.get(Calendar.DAY_OF_WEEK) == 1) {                      //если попали на вск., то перенос на пн.
-            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        // если месяцы выбранной в календаре по умолчанию даты и планируемой даты не равны
+        planningDate = generateDate(addDays, "MM");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        if (!generateDate(addDays, "MM").equals(generateDate(0, "MM"))) {
+            // то перелистываем страницу
+            $("[data-step='1']").click();
         }
-        String dd = Integer.toString(calendar.get(Calendar.DAY_OF_MONTH));  // наше новое число в текстовом формате
+        // используем поиск по коллекции элементов для нахождения даты в календаре
+        $$(".popup_visible .calendar__day").findBy(text(generateDate(addDays, "d"))).click();
 
-        DateFormat df = new SimpleDateFormat("MMM");                 // формат краткого текстового названия месяца с точкой на конце
-        String month = df.format(calendar.getTime());                       // наш новый месяц
-        month = month.substring(0, month.length() - 1);                     // убрать точку в конце строки
+        $("[data-test-id='name'] input").setValue("Василий");
+        $("[data-test-id='phone'] input").setValue("+79270000000");
+        $("[data-test-id='agreement']").click();
+        $(byText("Забронировать")).click();
 
-        $("[data-test-id='date'] input").click();                  // открыть инструмент календаря на странице
-        String calendarName = $("div.calendar__title > div.calendar__name").text().toLowerCase();
-        while (!calendarName.contains(month)) {
-            $x("//div[@class='calendar__arrow calendar__arrow_direction_right']").click(); // если не наш месяц, то переход в календаре на другой месяц
-        }                                                                   // выход из цикла, когда в календаре открыта страница нашего месяца
-        // теперь надо выбрать в календаре наше новое число
-        SelenideElement table = $("table.calendar__layout");
-        table.$(byText(dd)).click();                                        // дата через инструмент календаря выбрана
-        planningDate = $("[data-test-id='date'] input").getValue();
+        planningDate = generateDate(addDays, "dd.MM.yyyy");
 
-        form.$("[data-test-id='name'] input").setValue("Василий");
-        form.$("[data-test-id='phone'] input").setValue("+79270000000");
-        form.$("[data-test-id='agreement']").click();
-        form.$(byText("Забронировать")).click();
         $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $(".notification__content")
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
